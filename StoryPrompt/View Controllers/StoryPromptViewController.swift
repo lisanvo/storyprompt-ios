190e9888ad34ae9a2c2189098716138260fb0d82
Lisa Vo <lisa.vo97@gmail.com>
2021-11-26 20:58:59 -0800

    Embed in a navigation controller

--- StoryPrompt/View Controllers/StoryPromptViewController.swift
+++ StoryPrompt/View Controllers/StoryPromptViewController.swift
@@ -23,15 +23,11 @@ class StoryPromptViewController: UIViewController {
 		storyPromptTextView.text = storyPrompt.description
 	}
 	
+	override func viewWillAppear(_ animated: Bool) {
+		navigationController?.setNavigationBarHidden(true, animated: animated)
+	}
 	
-	/*
-	 // MARK: - Navigation
-	 
-	 // In a storyboard-based application, you will often want to do a little preparation before navigation
-	 override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-	 // Get the new view controller using segue.destination.
-	 // Pass the selected object to the new view controller.
-	 }
-	 */
-	
+	override func viewWillDisappear(_ animated: Bool) {
+		navigationController?.setNavigationBarHidden(false, animated: animated)
+	}
 }
